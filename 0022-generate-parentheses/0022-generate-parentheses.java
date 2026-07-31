ec379a80a2b53226a550d26d6767bf0516fd926d
abjeet yadav <avjeetyadav00@gmail.com>
2026-07-31 21:15:42 +0530

    Time: 0 ms (100%), Space: 44.4 MB (80.57%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
+        generate(res,n,0,0,new StringBuilder());
+        return res;
+    }
+
+    private void generate(List<String> res, int n, int open,int close, StringBuilder curr) {
+        if (curr.length()==n*2){
+            res.add(curr.toString());
+            return;
+        }
+        if (open<n){
+            curr.append("(");
+            generate(res,n,open+1,close,curr);
+            curr.deleteCharAt(curr.length() - 1);
+        }
+        if (close<open){
+            curr.append(")");
+            generate(res,n,open,close+1,curr);
+            curr.deleteCharAt(curr.length() - 1);
+        }
+    }
+}
