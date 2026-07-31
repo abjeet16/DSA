7f10178f3e49767e62c77962463874b078a0a0bd
abjeet yadav <avjeetyadav00@gmail.com>
2026-07-31 21:35:00 +0530

    Time: 0 ms (100%), Space: 48.2 MB (22.49%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,18 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if(n==0)
+            return 1.0;
+        if(n==-1){
+            return 1/x;
+        }
+        if(n==1)
+            return x;
+        double pow = myPow(x,n/2);
+        if(n%2!=0){
+            if(n<0)
+                return pow*pow*1/x;
+            return pow*pow*x;
+        }
+        return pow*pow;
+    }
+}
