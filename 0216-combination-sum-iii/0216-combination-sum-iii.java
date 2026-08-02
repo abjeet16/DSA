bcad1f621d195e96b030e8db0a33737a51cfa1b2
abjeet yadav <avjeetyadav00@gmail.com>
2026-08-02 17:45:25 +0530

    Time: 0 ms (100%), Space: 42.5 MB (30.1%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> res = new ArrayList<>();
+        generate(res,new ArrayList<Integer>(),1,k,n);
+        return res;
+    }
+
+    private void generate(List<List<Integer>> res, ArrayList<Integer> curr,int i , int numRem, int target) {
+        if (target==0&&numRem==0){
+            res.add(new ArrayList<>(curr));
+            return;
+        }
+        if (target<0||i==10)
+            return;
+        generate(res, curr, i+1, numRem, target);
+        curr.add(i);
+        generate(res, curr, i+1, numRem-1, target-i);
+        curr.removeLast();
+    }
+}
