94d2a3a05d154e3ae019dd0ab23c545feb0e0e17
abjeet yadav <avjeetyadav00@gmail.com>
2026-08-02 23:07:37 +0530

    Time: 7 ms (97.06%), Space: 65.6 MB (13.74%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -0,0 +1,33 @@
+class Solution {
+    public List<List<String>> partition(String s) {
+        List<List<String>> res = new ArrayList<>();
+        generate(res,new ArrayList<String>(),0,s);
+        return res;
+    }
+
+    private void generate(List<List<String>> res, ArrayList<String> curr, int idx, String s) {
+        if (idx==s.length()){
+            res.add(new ArrayList<>(curr));
+            return;
+        }
+        for (int i = idx; i < s.length() ;i++){
+            if(check(s,idx,i)){
+                curr.add(s.substring(idx,i+1));
+                generate(res, curr, i+1, s);
+                curr.removeLast();
+            }
+        }
+    }
+
+    private boolean check(String str , int i , int j){
+        if (str.isEmpty())
+            return false;
+        while (i<j){
+            if (str.charAt(i)!=str.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
