8932715ae22204d8701f579adb627618cc25c92d
abjeet yadav <avjeetyadav00@gmail.com>
2026-08-02 21:25:14 +0530

    Time: 4 ms (33%), Space: 41.9 MB (99.86%) - LeetHub

---
+++ 1935-maximum-number-of-words-you-can-type/1935-maximum-number-of-words-you-can-type.java
@@ -0,0 +1,21 @@
+class Solution {
+   public static int canBeTypedWords(String text, String brokenLetters) {
+        boolean[] isBroken = new boolean[26];
+        for (int i = 0; i < brokenLetters.length();i++){
+            isBroken[brokenLetters.charAt(i)-'a']=true;
+        }
+        int result = 0;
+        for (int i = 0 ; i < text.length() ; i++){
+            if (text.charAt(i) == ' '||(i==text.length()-1 && !isBroken[text.charAt(i) - 'a']))
+                result++;
+            else {
+                if (isBroken[text.charAt(i) - 'a']) {
+                    while (i < text.length() && text.charAt(i) != ' ') {
+                        i++;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
