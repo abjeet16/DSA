9222faf76f971cdb6e766687cf1c3232d1730ec5
abjeet yadav <avjeetyadav00@gmail.com>
2026-08-07 21:56:19 +0530

    Time: 51 ms (5.93%), Space: 50 MB (6.11%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,29 +1,30 @@
 class Solution {
-    public static int[][] merge(int[][] intervals) {
-        List<int[]> resList = new ArrayList<>();
-        Arrays.sort(intervals,(a,b)->{
-            return a[0]-b[0];
-        });
-        int start = intervals[0][0];
-        int end = intervals[0][1];
-        for (int[] i : intervals){
-            if (i[0]<=end){
-                if (i[1]>end){
-                    end=i[1];
-                }
+    public int[][] merge(int[][] in) {
+        Arrays.sort(in,(a,b)->a[0]==b[0]?Integer.compare(a[1],b[1]):Integer.compare(a[0],b[0]));
+        for(int[] i : in)
+            System.out.println(Arrays.toString(i));
+
+        int start = in[0][0];
+        int end = in[0][1];
+        List<int[]> res = new ArrayList<>();
+        
+        for(int i = 1 ; i < in.length ; i++){
+            if(in[i][0]>end){
+               
+                res.add(new int[]{start,end});
+                start = in[i][0];
+                end = in[i][1];
             }else{
-                int[] range = {start,end};
-                resList.add(range);
-                start = i[0];
-                end = i[1];
+                //System.out.println(start+" "+end);
+                if(end<in[i][1])end = in[i][1];
+                //System.out.println(start+" "+end);
             }
         }
-        int[] range = {start,end};
-        resList.add(range);
-        int len = resList.size();
-        int[][] res = new int[len][2];
-        for (int i = 0 ; i < len; i++)
-            res[i]=resList.get(i);
-        return res;
+        res.add(new int[]{start,end});
+        int[][] ans = new int[res.size()][2];
+        for(int i = 0 ; i < res.size() ; i++){
+            ans[i] = res.get(i);
+        }
+        return ans;
     }
 }
